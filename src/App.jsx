9e6f9bccdffb111fa4c849c68980636a02fe4b58
nakatatsu77 <nakatatsu1177@gmail.com>
2023-05-29 22:17:50 +0900

    jsxに書き換え

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import HamburgarMenu from "./HamburgarMenu";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React-HamburgarMenu</h1>
+      <HamburgarMenu />
+    </div>
+  );
+}
+
+export default App;
